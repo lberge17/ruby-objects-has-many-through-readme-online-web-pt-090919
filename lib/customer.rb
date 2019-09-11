e9de31ed9334835fcf993d20a4d9ad26137b5b31
Laura Berge <lauraeberge@gmail.com>
2019-09-11 22:10:47 +0000

    Automatically backed up by Learn

--- lib/customer.rb
+++ lib/customer.rb
@@ -8,12 +8,12 @@ class Customer
     @@all << self
   end
   
-  def self.all
-    @@all
-  end
-  
   def new_meal(waiter, total, tip = 0)
     Meal.new(waiter, self, total, tip)
   end
   
+  def self.all
+    @@all
+  end
+
 end
